6aa3219144da9937fa6233c7f17337a922caa92f
Alan Ross <alanross2006@gmail.com>
2019-06-16 14:20:47 +0100

    migrating to class fields

--- app/index.js
+++ app/index.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Popular from './components/Popular';
+import Battle from './components/Battle';
+import Results from './components/Results';
+import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light'
-        }))
-      }
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({theme}) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
     }
   }
+
   render() {
     return (
       <Router>
@@ -28,8 +26,12 @@ class App extends React.Component {
             <div className='container'>
               <Nav />
 
+              <Switch>
               <Route exact path='/' component={Popular} />
-              <Route path='/battle' component={Battle} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/battle/results' component={Results} />
+              <Route render={() => <h1>404</h1>} />
+              </Switch>
             </div>
           </div>
         </ThemeProvider>
